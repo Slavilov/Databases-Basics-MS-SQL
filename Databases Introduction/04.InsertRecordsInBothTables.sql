ee3ccd521cf2d50c4b8ed95893f59fc564eae88f
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-11-14 22:55:57 +0200

    Create 04.InsertRecordsInBothTables.sql

---
+++ Databases Introduction/04.InsertRecordsInBothTables.sql
@@ -0,0 +1,9 @@
+INSERT INTO Towns (Id, Name)
+     VALUES (1, 'Pleven'),
+            (2, 'Burgas'),
+            (3, 'Varna')
+
+INSERT INTO Minions (Id, Name, Age, TownId)
+     VALUES (1, 'Johnny', 28, 1),
+            (2, 'Amanda', 17, 3),
+            (3, 'Steward', NULL, 2)
