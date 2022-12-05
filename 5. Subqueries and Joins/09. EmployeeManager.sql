1149235bc913010a8784b3dd289aa5c35991f1c0
Velichko Slavilov <velichkoslavilov98@gmail.com>
2022-12-05 23:53:34 +0200

    Create 09. EmployeeManager.sql

---
+++ 5. Subqueries and Joins/09. EmployeeManager.sql
@@ -0,0 +1,3 @@
+SELECT emp.EmployeeID, emp.FirstName, emp.ManagerID, mng.FirstName AS ManagerName FROM Employees AS emp
+JOIN Employees AS mng ON mng.EmployeeID = emp.ManagerID
+WHERE emp.ManagerID IN ('3','7')
